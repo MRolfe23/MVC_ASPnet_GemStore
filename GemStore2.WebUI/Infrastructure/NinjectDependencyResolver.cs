183f4b35b2c68dbaf5a84892d77552a684d34da6
MRolfe23 <31248765+MRolfe23@users.noreply.github.com>
2018-04-18 21:24:21 -0500

    Add files via upload

---
+++ GemStore2.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+
+namespace GemStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel mykernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            mykernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type myserviceType)
+        {
+            return mykernel.TryGet(myserviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type myserviceType)
+        {
+            return mykernel.GetAll(myserviceType);
+        }
+
+        private void AddBindings()
+        {
+            /*
+                // we need to put bindings here
+                //This are moq (mock) implementation of IProductsRepository
+                Mock<IProductsRepository> myMock = new Mock<IProductsRepository>();
+                myMock.Setup(m => m.Products).Returns(new List<Product>{
+                    new Product {Name = "FOOTBALL", Price = 25 }
+                });
+                mykernel.Bind<IProductsRepository>().ToConstant(myMock.Object);
+            */
+           // mykernel.Bind<IProductsRepository>().To<EntityFrameWorkProductRepository>();
+        }
+    }
+}
